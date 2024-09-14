bdc048b395687a9ed41e9a1dc456c3ca26221535
AyumiShiiba <175018766+kinakonomoti@users.noreply.github.com>
2024-09-14 07:40:04 +0000

    [Finish]4

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,19 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
-  def create
-    book = Book.new(book_paramas)
-    book.save
-    redirect_to book_path(book.id)
+ def create
+  @book = Book.new(book_paramas)
+  if @book.save
+    flash[:notice] = "Book was successfully created."
+    redirect_to book_path(@book.id)
+  else
+    @books = Book.all
+    render :index
   end
+ end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -19,6 +22,18 @@ class BooksController < ApplicationController
 
   def edit
     @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_paramas)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  post 'books' => 'books#create'
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show', as: 'book'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  get 'homes/top'
-  get '/new/:id', to: 'books#new', as: 'new_book'
+  resources :books
+  root to: 'homes#top'
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
